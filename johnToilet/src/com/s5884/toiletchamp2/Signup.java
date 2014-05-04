0395cced74173c6d8d9651f433eb6f9dd6bf0aaf
john namu choi <namu_tree@hotmail.com>
2014-05-03 23:21:31 -0400

    namuToiletFirst

---
+++ johnToilet/src/com/s5884/toiletchamp2/Signup.java
@@ -0,0 +1,14 @@
+package com.s5884.toiletchamp2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Signup extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.signup);
+
+
+	}
+}
